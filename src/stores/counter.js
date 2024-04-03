0272485953e526893d85322495e0ed297812ca94
JJ <jjmendezlagunas@um.es>
2024-04-03 21:42:53 +0200

    pinia 1º

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -4,5 +4,13 @@ export const useCounterStore = defineStore({
     id: 'counter',
     state: () => ({
         count: 0
-    })
+    }),
+    actions: {
+        increaseCount(){
+            this.count++
+        },
+        decreaseCount(){
+            this.count--
+        }
+    }
 })
--- src/views/Mision.vue
+++ src/views/Mision.vue
@@ -1,3 +1,18 @@
 <template>
     <h1>Mision</h1>
+    <div class="count">
+        {{ storeCounter.count }}
+    </div>
+    <div class="buttons">
+        <button @click="storeCounter.decreaseCount">-</button>
+        <button @click="storeCounter.increaseCount">-</button>
+    </div>
 </template>
+
+<script setup>
+
+import { useCounterStore } from '@/stores/counter'
+
+const storeCounter = useCounterStore()
+
+</script>
