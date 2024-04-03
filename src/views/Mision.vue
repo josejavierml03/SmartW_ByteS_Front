6e648418b3edc6544d0ce210fc71372df075dbe6
JJ <jjmendezlagunas@um.es>
2024-04-03 22:14:25 +0200

    pinia Funciona

--- src/views/Mision.vue
+++ src/views/Mision.vue
@@ -1,12 +1,12 @@
 <template>
     <h1>Mision</h1>
-    <div class="count">
-        {{ storeCounter.count }}
-    </div>
-    <div class="buttons">
-        <button @click="storeCounter.decreaseCount">-</button>
-        <button @click="storeCounter.increaseCount">-</button>
-    </div>
+        <div class="count">
+            {{ storeCounter.count }}
+        </div>
+        <div class="buttons">
+            <button @click="storeCounter.decreaseCount" id="btR">-</button>
+            <button @click="storeCounter.increaseCount">+</button>
+        </div>
 </template>
 
 <script setup>
@@ -16,3 +16,18 @@ import { useCounterStore } from '@/stores/counter'
 const storeCounter = useCounterStore()
 
 </script>
+
+<style>
+.count{
+    margin-left: 30px;
+}
+
+.buttons{
+    margin-left: 20px;
+}
+
+#btR{
+    margin-right: 20px;
+}
+</style>
+
